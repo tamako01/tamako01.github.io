2cc012990d58c9bac5ff9b02c060c267a225050b
willfinyu <willfinyu@hotmail.com>
2023-02-24 22:06:25 -0800

    -add overlay

--- src/script_cursed.js
+++ src/script_cursed.js
@@ -26,12 +26,13 @@ $( document ).ready(function() {
     });
     $("#submit").click(function(e) {
         e.preventDefault();
-        if (count>5) {
-            window.close();
+        if (count>=5) {
+            window.location.href="main.html";
         }
         if ($("#answer").val()==="0225") {
             $("#dialog").hide();
             $("#reply").hide();
+            $("#overlay").css("background-color", "black");
             $("#end").show();
         } else if ($("#answer").val()==="你是谁？") {
             $("#reply").text("似乎我生前是有着奇妙力量的存在，不过我已经不记得，谁知道呢，说不定可能不是人类");
@@ -40,6 +41,8 @@ $( document ).ready(function() {
         } else if ($("#answer").val()==="灵幻新隆") {
             $("#reply").text("哦，那个人啊，似乎他与我的存在有着某种联系...想不起来");
         } else {
+            let op = 0.2*count;
+            $("#overlay").css("background-color", "rgba(255,0,0,"+op+")");
             count += 1;
             alert();
         }
